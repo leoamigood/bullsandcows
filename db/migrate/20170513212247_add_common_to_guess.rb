250512f697334c00b1f5d65bb555747513232132
Leo Amigood <leo@amigood.com>
2017-05-14 12:07:09 -0400

    Making migration non transactional

--- db/migrate/20170513212247_add_common_to_guess.rb
+++ db/migrate/20170513212247_add_common_to_guess.rb
@@ -1,10 +1,5 @@
 class AddCommonToGuess < ActiveRecord::Migration
   def change
     add_column :guesses, :common, :boolean
-
-    Guess.where(common: nil).each do |guess|
-      guess.common = Noun.exists?(noun: guess.word)
-      guess.save!
-    end
   end
 end
